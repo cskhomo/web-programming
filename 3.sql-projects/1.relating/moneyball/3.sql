13e0a8e12f9c864d839f0fd0bfdc1e28f00997fe
Sizwe Chris Khomo <164577349+cskhomo@users.noreply.github.com>
2024-09-02 02:10:46 +0200

    Update 3.sql

--- 3.sql-projects/1.relating/moneyball/3.sql
+++ 3.sql-projects/1.relating/moneyball/3.sql
@@ -2,4 +2,5 @@ SELECT "year", "HR"
 FROM "performances"
 JOIN "players" ON "player_id" = "players"."id"
 WHERE "first_name" = 'Ken' AND "last_name" = 'Griffey'
-AND "birth_year" = '1969';
+AND "birth_year" = '1969'
+ORDER BY year DESC;
